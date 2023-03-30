fd1ed591eec46c38671098263d63b56a9f0b4f18
Kamesh Sampath <kamesh.sampath@hotmail.com>
2023-03-30 12:58:45 +0530

    fix: agent image var name

--- infra/gitlab-gke-agent.tf
+++ infra/gitlab-gke-agent.tf
@@ -10,7 +10,7 @@ resource "helm_release" "gitlab_agent" {
 
   values = [
     templatefile("${path.module}/templates/values.tfpl", {
-      agentImageTag   = "${gitlab_agent_image_tag}"
+      agentImageTag   = "${var.gitlab_agent_image_tag}"
       agentKasAddress = "${var.gitlab_kas_address}"
       agentReplicas   = "${var.gitlab_agent_replicas}"
       agentToken      = "${var.gitlab_agent_token}"
